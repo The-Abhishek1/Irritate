4206af7f744a4d8961d7bc81cd8b841517b6ea43
The-Abhishek1 <idiot63666@gmail.com>
2024-07-07 18:08:33 +0530

    Positioned Elements neatly

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import p5 from "@public/bindu2.png";
 //Main Function
 export default function Home() {
   return (
-    <div className="flex italic text-center flex-col gap-3 p-3 h-[100%] w-[100%]">
+    <div className="flex flex-col py-2 pb-14 italic text-center gap-3 p-3 h-[100%] w-[100%]">
       <div className="flex flex-row items-center justify-center">
         <h1 className="font-bold  uppercase text-[20px] italic">
           Nothing Just Wanted to Say{" "}
@@ -29,32 +29,41 @@ export default function Home() {
           </p>
         </h4>
       </div>
-      <div className="flex gap-4 flex-col items-center justify-center">
-        <Image src={p1} alt="She" className="max-w-[400px] msmmm:w-[300px]" />
-        <div className="flex flex-row">
-          <Image
-            src={p4}
-            alt="She"
-            className="max-w-[200px] msmmm:w-[150px] max-h-[200px]"
-          />
-          <Image
-            src={p5}
-            alt="She"
-            className="max-w-[200px] msmmm:w-[150px] max-h-[200px]"
-          />
-        </div>
-        <h3>Compare the above clips🤣🤣🤣</h3>
+      <div className="lg:flex items-center gap-20 justify-center">
+        <div className="flex gap-4 flex-col items-center justify-center">
+          <Image src={p1} alt="She" className="max-w-[400px] msmmm:w-[300px]" />
+          <div className="flex flex-row">
+            <Image
+              src={p4}
+              alt="She"
+              className="max-w-[200px] msmmm:w-[150px] max-h-[200px]"
+            />
+            <Image
+              src={p5}
+              alt="She"
+              className="max-w-[200px] msmmm:w-[150px] max-h-[200px]"
+            />
+          </div>
+          <h3 className="lg:hidden">Compare the above clips🤣🤣🤣</h3>
 
-        <Image src={p2} alt="She" className="max-w-[400px]" />
-      </div>
-      <div className="flex items-center text-center flex-col justify-center text-green-500">
-        <h3>And I'm still 6 month's Kid Discovering Hiiden Facts</h3>
-        <h3>Don't worry, You can Thank Me Later👋💤😂🤣😂</h3>
-        <Image src={p3} alt="Me" className="w-[60px] mt-2 h-[60px] " />
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <h3>Goooo Study Now🐒👋👋</h3>
-        <h3 className="font-bold text-red-600">SEE YOU LATER AUNTY😀😀</h3>
+          <Image src={p2} alt="She" className="max-w-[400px] lg:hidden" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="mlg:hidden">
+            <h3>Compare the Side clips🤣🤣🤣</h3>
+
+            <Image src={p2} alt="She" className="max-w-[400px]" />
+          </div>
+          <div className="flex mlg:pt-6 items-center text-center flex-col justify-center text-green-500">
+            <h3>And I'm still 6 month's Kid Discovering Hiiden Facts</h3>
+            <h3>Don't worry, You can Thank Me Later👋💤😂🤣😂</h3>
+            <Image src={p3} alt="Me" className="w-[60px] mt-2 h-[60px] " />
+          </div>
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <h3>Goooo Study Now🐒👋👋</h3>
+            <h3 className="font-bold text-red-600">SEE YOU LATER AUNTY😀😀</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
